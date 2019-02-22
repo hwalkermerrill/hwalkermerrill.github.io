58ad8d7df3d613ce1ddaa9b23247af2e8d980bd3
hwalkermerrill <hwalkermerrill@gmail.com>
2019-02-22 15:38:47 +0900

    fix missing close paren

--- lesson-5/js/windchill.js
+++ lesson-5/js/windchill.js
@@ -1,3 +1,3 @@
 function windchill(tempF, speed){
     var windTemp = 35.74 + (0.6215 * tempF) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * tempF * (Math.pow(speed, 0.16)));
-    document.write(windTemp);
+    document.write(windTemp)};
