05d8fc94927b26dd2d9b0227b1c7c84029b47aa6
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-14 12:29:35 +0900

    fixback

--- JSON/js/fillJSON.js
+++ JSON/js/fillJSON.js
@@ -3,9 +3,9 @@ window.onload = fillJSON;
 function fillJSON(){
     var header = document.querySelector('header');
     var section = document.querySelector('section');
-    var requestSRC = 'JSON\json\superheroes.json';
+    var requestURL = 'JSON\json\superheroes.json';
     var request = new XMLHttpRequest();
-    request.open('GET', requestSRC);
+    request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
     request.onload = function() {
