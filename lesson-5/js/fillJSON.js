2d975564ff7a8a3404d93b7afa38e1a06d4f9a37
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-14 14:11:59 +0900

    update link reference

--- lesson-5/js/fillJSON.js
+++ lesson-5/js/fillJSON.js
@@ -3,7 +3,7 @@ window.onload = fillJSON;
 function fillJSON(){
     //var header = document.querySelector('header');
     var section = document.querySelector('section');
-    var requestURL = 'towndata.json';
+    var requestURL = 'js/towndata.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
