7485d8dcb47cd01aa53bbc6715ad6bd1287622cc
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-14 16:53:35 +0900

    added gallery hover border, updated motto line

--- lesson-5/js/fillJSON.js
+++ lesson-5/js/fillJSON.js
@@ -28,7 +28,7 @@ function fillJSON(){
         for (var i = 0; i < towns.length; i++) {
             var myArticle = document.createElement('article');
             var myH2 = document.createElement('h2');
-            var myPara1 = document.createElement('p');
+            var myH3 = document.createElement('h3');
             var myPara2 = document.createElement('p');
             var myPara3 = document.createElement('p');
             var myPara4 = document.createElement('p');
@@ -60,7 +60,7 @@ function fillJSON(){
             }
 
             myH2.textContent = towns[i].name;
-            myPara1.textContent = 'Motto: ' + towns[i].motto;
+            myH3.textContent = '"' + towns[i].motto + '"';
             myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
             myPara3.textContent = 'Current Population: ' + towns[i].currentPopulation;
             myPara4.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
@@ -74,7 +74,7 @@ function fillJSON(){
             }
 
             myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myH3);
             myArticle.appendChild(myPara2);
             myArticle.appendChild(myPara3);
             myArticle.appendChild(myPara4);
