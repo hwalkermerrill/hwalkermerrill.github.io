1f4398d7cc54638bf9978067785958f0409ca62a
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-22 02:41:50 +0900

    added seperate forecast date .js

--- weathersitefinal/js/currentdate.js
+++ weathersitefinal/js/currentdate.js
@@ -41,26 +41,3 @@ if (mm < 4) {
 
 today = dw + ", " + dd + " " + mm + " " + yyyy;
 document.write(today);
-
-function forecastDate(x) {
-    var today = new Date();
-    var dw = today.getDay() + 1 + x;
-    var swap;
-
-    if (dw > 7) {
-        swap = dw - 7;
-        dw = swap;
-    }
-
-    if (dw < 4) {
-        if (dw == 1) {dw = "Monday"}
-    else if (dw == 2) {dw = "Tuesday"}
-    else dw = "Wednesday"
-    }
-    else if (dw < 7) {
-        if (dw == 4) {dw = "Thursday"}
-        else if (dw == 5) {dw = "Friday"}
-        else dw = "Saturday"
-    }
-    else dw = "Sunday"
-}
---
+++ weathersitefinal/js/forecastDate.js
@@ -0,0 +1,24 @@
+function forecastDate(x) {
+    var today = new Date();
+    var dw = today.getDay() + 1 + x;
+    var swap;
+
+    if (dw > 7) {
+        swap = dw - 7;
+        dw = swap;
+    }
+
+    if (dw < 4) {
+        if (dw == 1) {dw = "Monday"}
+    else if (dw == 2) {dw = "Tuesday"}
+    else dw = "Wednesday"
+    }
+    else if (dw < 7) {
+        if (dw == 4) {dw = "Thursday"}
+        else if (dw == 5) {dw = "Friday"}
+        else dw = "Saturday"
+    }
+    else dw = "Sunday"
+
+    return dw
+}
