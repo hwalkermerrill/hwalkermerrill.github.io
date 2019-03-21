b18ca1a1592da57bf26a1a787ec36d0412fa5f95
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-22 00:13:32 +0900

    added colon and naming to function

--- weathersitefinal/js/getweather.js
+++ weathersitefinal/js/getweather.js
@@ -1,12 +1,12 @@
-function currentWeather(x){
-    var cid
+function currentWeather(x) {
+    var cid;
     if (x == 1) {cid = 5604473} // preston
     else if (x == 2) {cid = 5678757} // soda springs
     else if (x == 3) {cid = 5585010} // fish haven  
     var weatherObject = new XMLHttpRequest;
     weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?id=' + cid + '&appid=fb75400a87c1c698878761e1d3548782&units=imperial',true);
     weatherObject.send();
-    weatherObject.onload = function() {
+    weatherObject.onload = function pullWeather() {
         var weatherInfo = JSON.parse(weatherObject.responseText);
         console.log(weatherInfo);
 
