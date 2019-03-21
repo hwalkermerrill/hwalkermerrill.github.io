02d3c65aae55b279cfff2277c4e77d10612ae8d9
hwalkermerrill <hwalkermerrill@gmail.com>
2019-03-22 01:28:09 +0900

    change formula to include windspeed

--- weathersitefinal/js/getweather.js
+++ weathersitefinal/js/getweather.js
@@ -11,14 +11,18 @@ function currentWeather(x) {
         console.log(weatherInfo);
 
         document.getElementById('currentType').innerHTML = weatherInfo.weather[0].description;
-        document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
         document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
         document.getElementById('lowTemp').innerHTML = weatherInfo.main.temp_min;
         document.getElementById('highTemp').innerHTML = weatherInfo.main.temp_max;
-        document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
 
         var iconcode = weatherInfo.weather[0].icon;
         var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
         document.getElementById('weather_icon').src = icon_path;
+
+        // Feels Like - Wind Chill
+        var tempF = parseFloat(document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp);
+        var windSpeed = parseFloat(document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed);
+        var windTemp = 35.74 + (0.6215 * tempF) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 * tempF * (Math.pow(windSpeed, 0.16)));
+        document.getElementById('windChill').innerHTML = Math.round(windTemp, 1)
 
 }} // end of function
