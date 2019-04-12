d97ed95fd640785a92e083cbe02e37eadb4ed093
hwalkermerrill <hwalkermerrill@gmail.com>
2019-04-13 01:01:06 +0900

    update maintemp and current type with numbers

--- templesitefinal/js/getweather.js
+++ templesitefinal/js/getweather.js
@@ -9,11 +9,32 @@ function currentWeather(x) {
     var weatherObject = new XMLHttpRequest;
     weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&appid=fb75400a87c1c698878761e1d3548782&units=imperial',true);
     weatherObject.send();
-    weatherObject.onload = function pullWeather() {
+    weatherObject.onload = function pullWeather(x) {
         var weatherInfo = JSON.parse(weatherObject.responseText);
         console.log(weatherInfo);
-
-        document.getElementById('currentType').innerHTML = weatherInfo.weather[0].description;
-        document.getElementById('mainTemp').innerHTML = Math.round(weatherInfo.main.temp);
+        if (x == 1) /* noblesville indiana */ {
+            document.getElementById('currentType1').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp1').innerHTML = Math.round(weatherInfo.main.temp);
+        }
+        else if (x == 2) /* laie hawaii */ {
+            document.getElementById('currentType2').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp2').innerHTML = Math.round(weatherInfo.main.temp);
+        }
+        else if (x == 3) /* crestwood kentucky */ {
+            document.getElementById('currentType3').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp3').innerHTML = Math.round(weatherInfo.main.temp);
+        }
+        else if (x == 4) /* nauvoo illinois */ {
+            document.getElementById('currentType4').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp4').innerHTML = Math.round(weatherInfo.main.temp);
+        }
+        else if (x == 5) /* redlands california */ {
+            document.getElementById('currentType5').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp5').innerHTML = Math.round(weatherInfo.main.temp);
+        }
+        else if (x == 6) /* omaha nebraska */ {
+            document.getElementById('currentType6').innerHTML = weatherInfo.weather[0].description;
+            document.getElementById('mainTemp6').innerHTML = Math.round(weatherInfo.main.temp);
+        }
     }
 } // end of function
