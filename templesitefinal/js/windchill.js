cc3bd567b89dd21d63147879823c4038cbaf764b
hwalkermerrill <hwalkermerrill@gmail.com>
2019-04-13 00:13:59 +0900

    remove unnecessary files

--- templesitefinal/js/windchill.js
+++
@@ -1,4 +0,0 @@
-function windchill(tempF, speed){
-    var windTemp = 35.74 + (0.6215 * tempF) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * tempF * (Math.pow(speed, 0.16)));
-    windTemp = Math.round(windTemp, 1)
-    document.write(windTemp)};
