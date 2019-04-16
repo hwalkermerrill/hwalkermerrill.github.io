409d39427c962c28385773a46a69fe7d1739e42a
hwalkermerrill <hwalkermerrill@gmail.com>
2019-04-16 17:38:17 +0900

    test update entire script to persona

--- war4crown/js/fillJSON.js
+++ war4crown/js/fillJSON.js
@@ -1,7 +1,7 @@
 function fillJSON(divClass){
   var sl = 'main .' + divClass;
   var section = document.querySelector(sl);
-  var requestURL = 'js/templedata.json';
+  var requestURL = 'js/personadb.json';
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
@@ -17,103 +17,52 @@ function fillJSON(divClass){
       var myArticle = document.createElement('article');
       var myH2 = document.createElement('h2');
       var myH3 = document.createElement('h3');
+      var myPara1 = document.createElement('p');
       var myPara2 = document.createElement('p');
       var myPara3 = document.createElement('p');
       var myPara4 = document.createElement('p');
-      var myPara5 = document.createElement('p');
-      var myList1 = document.createElement('ul');
-      var myPara6 = document.createElement('p');
-      var myList2 = document.createElement('ul');
-      var myPara7 = document.createElement('p');
-      var myList3 = document.createElement('ul');
-      var myPara8 = document.createElement('p');
-      var myList4 = document.createElement('ul');
-      var myPara9 = document.createElement('p');
-      var myList5 = document.createElement('ul');
       var myIMG = document.createElement('img');
+      var myList1 = document.createElement('ul');
 
-      if (localJObj[i].name == "Indianapolis Indiana Temple") {
-        myIMG.setAttribute('src', 'images/indianapolis-indiana-720w.jpg');
-        myIMG.setAttribute('alt', 'Indianapolis');
-      } else if (localJObj[i].name == "Laie Hawaii Temple") {
-        myIMG.setAttribute('src', 'images/laie-hawaii-720w.jpg');
-        myIMG.setAttribute('alt', 'Hawaii');
-      } else if (localJObj[i].name == "Louisville Kentucky Temple") {
-        myIMG.setAttribute('src', 'images/louisville-kentucky-720w.jpg');
-        myIMG.setAttribute('alt', 'Kentucky');
-      } else if (localJObj[i].name == "Nauvoo Illinois Temple") {
-        myIMG.setAttribute('src', 'images/nauvoo-illinois-720w.jpg');
-        myIMG.setAttribute('alt', 'Nauvoo');
-      } else if (localJObj[i].name == "Redlands California Temple") {
-        myIMG.setAttribute('src', 'images/redlands-california-720w.jpg');
-        myIMG.setAttribute('alt', 'Redlands');
-        } else {
-        myIMG.setAttribute('src', 'images/winter-quarters-nebraska-720w.jpg');
-        myIMG.setAttribute('alt', 'Winter Quarters');
+      if (divClass == "dead" || "dpc") {
+        myIMG.setAttribute('src', 'images/unknown.jpg');
+        myIMG.setAttribute('alt', 'Unknown');
+      } else {
+        myIMG.setAttribute('src', localJObj[i].imgref);
+        myIMG.setAttribute('alt', localJObj[i].name);
       }
 
-      myH2.textContent = localJObj[i].name;
-      myH3.textContent = 'Est. ' + localJObj[i].history[2];
-      myPara2.textContent = 'Address: ' + localJObj[i].address;
-      myPara3.textContent = 'Telephone: ' + localJObj[i].telephone;
-      myPara4.textContent = 'Email: ' + localJObj[i].email;
-      myPara5.textContent = 'Services: ';
-      myPara6.textContent = 'Ordinances: ';
-      myPara7.textContent = 'Endowment Schedule: ';
-      myPara8.textContent = 'Closure Schedule: ';
-      myPara9.textContent = 'History: ';
+      var listSkill = document.createElement('li');
+      listSkill.textContent = 'Influenced by ' + localJObj[i].skills;
+      myList1.appendChild(listSkill);
 
-      var services = localJObj[i].services;
-      for (var j = 0; j < services.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = services[j];
-        myList1.appendChild(listItem);
-      }
+      var listBias = document.createElement('li');
+      listBias.textContent = 'Known Biases: ' + localJObj[i].biases;
+      myList1.appendChild(listBias);
 
-      var ordSched = localJObj[i].ordSched;
-      for (var j = 0; j < ordSched.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = ordSched[j];
-        myList2.appendChild(listItem);
-      }
+      var listStrength = document.createElement('li');
+      listStrength.textContent = 'Resistant towards: ' + localJObj[i].strengths;
+      myList1.appendChild(listStrength);
 
-      var sesSched = localJObj[i].sesSched;
-      for (var j = 0; j < sesSched.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = sesSched[j];
-        myList3.appendChild(listItem);
-      }
+      var listWeak = document.createElement('li');
+      listWeak.textContent = 'Sympathetic towards: ' + localJObj[i].weakness;
+      myList1.appendChild(listWeak);
 
-      var closureSchedule = localJObj[i].closureSchedule;
-      for (var j = 0; j < closureSchedule.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = closureSchedule[j];
-        myList4.appendChild(listItem);
-      }
-
-      var history = localJObj[i].history;
-      for (var j = 0; j < history.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = history[j];
-        myList5.appendChild(listItem);
-      }
+      myH2.textContent = localJObj[i].announce;
+      myH3.textContent = localJObj[i].title + localJObj[i].name[i];
+      myPara1.textContent = 'Appearance: ' + localJObj[i].appearance;
+      myPara2.textContent = 'Background: ' + localJObj[i].background;
+      myPara3.textContent = 'Cause of Death: ' + localJObj[i].email;
+      myPara4.textContent = 'Social Skills: ';
 
       myArticle.appendChild(myH2);
       myArticle.appendChild(myH3);
       myArticle.appendChild(myIMG);
+      myArticle.appendChild(myPara1);
       myArticle.appendChild(myPara2);
       myArticle.appendChild(myPara3);
       myArticle.appendChild(myPara4);
-      myArticle.appendChild(myPara5);
       myArticle.appendChild(myList1);
-      myArticle.appendChild(myPara6);
-      myArticle.appendChild(myList2);
-      myArticle.appendChild(myPara7);
-      myArticle.appendChild(myList3);
-      myArticle.appendChild(myPara8);
-      myArticle.appendChild(myList4);
-      myArticle.appendChild(myPara9);
-      myArticle.appendChild(myList5);
 
       section.appendChild(myArticle);
     }
