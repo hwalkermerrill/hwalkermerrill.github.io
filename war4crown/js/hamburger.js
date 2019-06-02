71216f9dba8f4631a03e2873a1eaf6d37e2dda09
hwalkermerrill <hwalkermerrill@gmail.com>
2019-06-03 01:07:57 +0900

    test new button format for toggle visible

--- war4crown/js/hamburger.js
+++ war4crown/js/hamburger.js
@@ -1,8 +1,17 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-function toggleLocked() {
-  document.getElementsByClassName("locked").classList.toggle("hide");
+function toggleVisible(className) {
+  var elements = document.getElementsByClassName(document, className);
+  n = elements.length;
+  for (var i = 0; i < n; i++) {
+    var e = elements[i]
+    if(e.getElementsByClassName.display == 'block') {
+      e.getElementsByClassName.display = 'none';
+    } else {
+      e.getElementsByClassName.display = 'block';
+    }
+  }
 }
 $(window).on("load", function(){
   $("ul").click(function(){
