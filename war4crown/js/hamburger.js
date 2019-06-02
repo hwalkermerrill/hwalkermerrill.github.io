4ac33d48c4d12788f905288c37cd2402be4b9fa1
hwalkermerrill <hwalkermerrill@gmail.com>
2019-06-03 01:31:29 +0900

    test switch to jQuerry call

--- war4crown/js/hamburger.js
+++ war4crown/js/hamburger.js
@@ -1,12 +1,17 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-function toggleVisible(className) {
+/*function toggleVisible(className) {
   className = className 
   document.getElementsByClassName("locked").classList.toggle("hide");
-}
+}*/
 $(window).on("load", function(){
   $("ul").click(function(){
        $("li", this).toggle();
   });
 });
+function toggleVisible(className) {
+  $(function() {
+    $(className).toggleClass( "hide" );
+  })
+}
