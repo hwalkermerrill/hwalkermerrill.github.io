f007ef14fce9902fedf2f4560b7180b5fc19c4c9
hwalkermerrill <hwalkermerrill@gmail.com>
2019-06-03 01:19:57 +0900

    test revisit formula

--- war4crown/js/hamburger.js
+++ war4crown/js/hamburger.js
@@ -2,16 +2,7 @@ function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 function toggleVisible(className) {
-  var elements = document.getElementsByClassName(className);
-  n = elements.length;
-  for (var i = 0; i < n; i++) {
-    var e = elements[i]
-    if(e.getElementsByClassName.display == 'block') {
-      e.getElementsByClassName.display = 'none';
-    } else {
-      e.getElementsByClassName.display = 'block';
-    }
-  }
+  document.getElementsByClassName(className).classList.toggle("hide");
 }
 $(window).on("load", function(){
   $("ul").click(function(){
