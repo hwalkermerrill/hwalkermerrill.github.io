404c3c467fb6a4d4bb8b96da86669604d03df740
hwalkermerrill <hwalkermerrill@gmail.com>
2019-06-03 00:49:11 +0900

    add togle button to hide unavailable

--- war4crown/js/hamburger.js
+++ war4crown/js/hamburger.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
+function toggleLocked() {
+  document.getElementsByClassName("locked").classList.toggle("hide");
+}
 $(window).on("load", function(){
   $("ul").click(function(){
        $("li", this).toggle();
