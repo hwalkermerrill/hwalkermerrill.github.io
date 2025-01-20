2f813e076e2712c8f1bde4e61e931f9e10a0301c
Harrison Merrill <hwalkermerrill@gmail.com>
2025-01-21 03:16:45 +0900

    add comments and reduce noise

--- wdd131/mission/scripts/script.js
+++ wdd131/mission/scripts/script.js
@@ -1,16 +1,18 @@
 // This functions toggles the theme between light and dark mode
 
+// This section defines variables to make the function easier to read.
 let themeSelector = document.getElementById("theme");
 let logo = document.getElementById("logo");
 
 function changeTheme() {
-    let theme = document.getElementById("theme").value;
+    let theme = themeSelector.value;
+    // This loop swaps the documents theme to dark if selected, otherwise it leaves it light.
     if (theme === "dark") {
         document.body.className = "dark";
-        document.getElementById("logo").src = "images/byui-logo_white.png";
+        logo.src = "images/byui-logo_white.png";
     } else {
         document.body.className = "light";
-        document.getElementById("logo").src = "images/byui-logo_blue.webp";
+        logo.src = "images/byui-logo_blue.webp";
     }
 }
 themeSelector.addEventListener("change", changeTheme);
