d4d3cf8600911cd55da3182ceb9051fed7d70d89
Harrison Merrill <hwalkermerrill@gmail.com>
2025-02-06 14:59:01 +0900

    create water_flow.py assignment

---
+++ cse111/water_flow.py
@@ -0,0 +1,19 @@
+# Written by Harrison Merrill 2/6/25 CSE111
+
+# Problem Statement:
+# Getting clean water to all buildings in a city is a large and expensive task.
+# Many cities will build an elevated water tank, and install a pump that pushes water
+# up to the tank where the water is stored. In the city, when a thirsty person opens
+# a water faucet, water runs from the tank through pipes to the faucet. Earth’s gravity
+# pulling on the water in the elevated tank pressurizes the water and causes it to spray
+# from the faucet.
+# >
+# Before a city builds a water distribution system, an engineer must design the system
+# and ensure water will flow to all buildings in the city. An engineer must choose the
+# tower height, pipe type, pipe diameter, and pipe path. Engineers use software to help
+# them make these choices and design a working water distribution system.
+
+# Assignment
+# Write a Python program that could help an engineer design a water distribution system.
+# During this prove milestone, you will write three program functions and three test
+# functions as described in the Steps section below.
