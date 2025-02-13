a4559943bde7b40a5f6940b8b51ff43ccf4aa13d
Harrison Merrill <hwalkermerrill@gmail.com>
2025-02-13 11:36:13 +0900

    finish weekly project

--- cse111/test_water_flow.py
+++ cse111/test_water_flow.py
@@ -7,6 +7,10 @@ from water_flow import (
     water_column_height,
     pressure_gain_from_water_height,
     pressure_loss_from_pipe,
+    pressure_loss_from_fittings,
+    reynolds_number,
+    pressure_loss_from_pipe_reduction,
+    pressure_at_house,
 )
 
 
@@ -69,6 +73,51 @@ def test_pressure_loss_from_pipe():
         friction_factor=0.013,
         fluid_velocity=1.65,
     ) == approx(-110.884, abs=0.001)
+
+
+def test_pressure_loss_from_fittings():
+    # asserts use approx to set tolerance to 0.001 of float
+    assert pressure_loss_from_fittings(0, 3) == approx(0, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+
+
+def test_reynolds_number():
+    # asserts use approx to set tolerance to 1 of float
+    assert reynolds_number(0.048692, 0) == approx(0, abs=1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
+    assert reynolds_number(0.28687, 1.65) == approx(471729, abs=1)
+    assert reynolds_number(0.28687, 1.75) == approx(500318, abs=1)
+
+
+def test_pressure_loss_from_pipe_reduction():
+    # asserts use approx to set tolerance to 0.001 of float
+    assert pressure_loss_from_pipe_reduction(
+        larger_diameter=0.28687,
+        fluid_velocity=0,
+        reynolds_number=1,
+        smaller_diameter=0.048692,
+    ) == approx(0, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(
+        larger_diameter=0.28687,
+        fluid_velocity=1.65,
+        reynolds_number=471729,
+        smaller_diameter=0.048692,
+    ) == approx(-163.744, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(
+        larger_diameter=0.28687,
+        fluid_velocity=1.75,
+        reynolds_number=500318,
+        smaller_diameter=0.048692,
+    ) == approx(-184.182, abs=0.001)
+
+
+def test_pressure_at_house():
+    # asserts use approx to set tolerance to 0.1 of float
+    assert pressure_at_house(36.6, 9.1, 1524.0, 3, 15.2) == approx(158.7, abs=0.1)
 
 
 # ----- Main Function Runs -----
--- cse111/water_flow.py
+++ cse111/water_flow.py
@@ -24,6 +24,16 @@
 # ----- Constants -----
 density_of_water = 998.2  # kg/m^3
 gravity = 9.80665  # m/s^2
+water_dynamic_viscosity = 0.0010016  # N*s/m^2
+
+# ----- Variables Copied from cse111 lesson06 -----
+PVC_SCHED80_INNER_DIAMETER = 0.28687  # (meters)  11.294 inches
+PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
+SUPPLY_VELOCITY = 1.65  # (meters / second)
+
+HDPE_SDR11_INNER_DIAMETER = 0.048692  # (meters)  1.917 inches
+HDPE_SDR11_FRICTION_FACTOR = 0.018  # (unitless)
+HOUSEHOLD_VELOCITY = 1.75  # (meters / second)
 
 
 # ---- Function Definitions -----
@@ -48,3 +58,90 @@ def pressure_loss_from_pipe(
         -friction_factor * pipe_length * density_of_water * fluid_velocity**2
     ) / (2000 * pipe_diameter)
     return pressure_loss
+
+
+# This function calculates pressure loss from fittings
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    pressure_loss_fittings = (
+        -0.04 * density_of_water * fluid_velocity**2 * quantity_fittings
+    ) / 2000
+    return pressure_loss_fittings
+
+
+# This functions returns the reynolds number for a pipe with water flowing through it
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    reynolds = (
+        density_of_water * fluid_velocity * hydraulic_diameter
+    ) / water_dynamic_viscosity
+    return reynolds
+
+
+# This function calculates the water pressure lost from water moving from a
+# larger diameter pipe to a smaller diameter pipe
+def pressure_loss_from_pipe_reduction(
+    larger_diameter, fluid_velocity, reynolds_number, smaller_diameter
+):
+    reduction_constant = (0.1 + (50 / reynolds_number)) * (
+        ((larger_diameter / smaller_diameter) ** 4) - 1
+    )
+    pressure_loss_reduction = (
+        -reduction_constant * density_of_water * fluid_velocity**2
+    ) / 2000
+    return pressure_loss_reduction
+
+
+def pressure_at_house(tower_height, tank_height, length1, quantity_angles, length2):
+    water_height = water_column_height(tower_height, tank_height)
+    pressure = pressure_gain_from_water_height(water_height)
+    reynolds = reynolds_number(
+        hydraulic_diameter=PVC_SCHED80_INNER_DIAMETER, fluid_velocity=SUPPLY_VELOCITY
+    )
+
+    loss = pressure_loss_from_pipe(
+        pipe_diameter=PVC_SCHED80_INNER_DIAMETER,
+        pipe_length=length1,
+        friction_factor=PVC_SCHED80_FRICTION_FACTOR,
+        fluid_velocity=SUPPLY_VELOCITY,
+    )
+    pressure += loss
+
+    loss = pressure_loss_from_fittings(
+        fluid_velocity=SUPPLY_VELOCITY, quantity_fittings=quantity_angles
+    )
+    pressure += loss
+
+    loss = pressure_loss_from_pipe_reduction(
+        larger_diameter=PVC_SCHED80_INNER_DIAMETER,
+        fluid_velocity=SUPPLY_VELOCITY,
+        reynolds_number=reynolds,
+        smaller_diameter=HDPE_SDR11_INNER_DIAMETER,
+    )
+    pressure += loss
+
+    loss = pressure_loss_from_pipe(
+        pipe_diameter=HDPE_SDR11_INNER_DIAMETER,
+        pipe_length=length2,
+        friction_factor=HDPE_SDR11_FRICTION_FACTOR,
+        fluid_velocity=HOUSEHOLD_VELOCITY,
+    )
+    pressure += loss
+
+    return pressure
+
+
+def main():
+    tower_height = float(input("Height of water tower (meters): "))
+    tank_height = float(input("Height of water tank walls (meters): "))
+    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
+    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
+    length2 = float(input("Length of pipe from supply to house (meters): "))
+
+    pressure = pressure_at_house(
+        tower_height, tank_height, length1, quantity_angles, length2
+    )
+
+    print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+
+if __name__ == "__main__":
+    main()
