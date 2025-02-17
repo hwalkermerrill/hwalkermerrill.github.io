2c091d590071dcc44c8b840fce9fef58767efe3c
Harrison Merrill <hwalkermerrill@gmail.com>
2025-02-17 19:38:32 +0900

    array demo complete

--- wdd131/sandbox/array.js
+++ wdd131/sandbox/array.js
@@ -2,6 +2,11 @@
 
 const steps = ["one", "two", "three"];
 const grades = ["A", "B", "A"]
+const words = ["watermelon", "peach", "apple", "tomato", "grape"];
+const myArray = [12, 34, 21, 54];
+const luckyNumber = 21;
+let luckyIndex = myArray.indexOf(luckyNumber);
+
 const listTemplate = (step) => `${step}`;
 
 const stepsHtml = steps.map(listTemplate);
@@ -18,6 +23,7 @@ function convertGradeToPoints(grade) {
 }
 const gpaPoints = grades.map(convertGradeToPoints);
 const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
+const shortWords = words.filter((word) => word.length < 6);
 
 // let new_array = arr.map(function callback(currentValue[, index[, array]]) {
 //     // return element for new_array
