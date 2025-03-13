0407af19f916eb7dc3e498a04cef3d6857c577d3
Harrison Merrill <hwalkermerrill@gmail.com>
2025-03-13 12:50:54 +0900

    finish receipt.py

--- cse111/unit_5/receipt.py
+++ cse111/unit_5/receipt.py
@@ -11,6 +11,15 @@
 # Imports
 import csv
 from decimal import Decimal
+from datetime import datetime
+
+# import sys
+
+# Constants
+STORE_NAME = "Merrily Shopping"
+SIGNATURE_LINE = "Thank you for shopping with us! Have a great day!"
+SALES_TAX = 6
+current_date_and_time = datetime.now()
 
 
 def read_dictionary(filename, key_column_index):
@@ -24,77 +33,165 @@ def read_dictionary(filename, key_column_index):
     Return: a compound dictionary that contains
             the contents of the CSV file.
     """
-    products_dict = {}
+    output_dict = {}
 
-    with open(filename, mode="r") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
+    try:
+        with open(filename, mode="r") as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
 
-        for row in reader:
-            key = row[key_column_index]
-            value = [row[0], row[1], float(row[2])]
-            products_dict[key] = value  # Write values into dictionary
+            for row in reader:
+                try:
+                    key = row[key_column_index]
+                    value = [row[0], row[1], Decimal(row[2])]
+                    output_dict[key] = value  # Write values into dictionary
+                except (IndexError, ValueError) as error:
+                    continue  # Skip badly formatted rows, intentionally printing nothing
+                    # on the receipt to add error messages on receipts that do not contain
+                    # that item. If their purchase does contain the item, this will be
+                    # reported later anyway.
+        return output_dict
 
-    return products_dict
+    except FileNotFoundError:
+        print(f"File {filename} not found. Please check the filename and try again.")
+        return
+        # sys.exit(1)
 
 
-def read_request(request, products_dict, sales_tax_percentage=0):
+def read_request(
+    request, products_dict, sales_tax_percentage=6, special_discounts=None
+):
     subtotal = 0
+    total_count = 0
+    discount_total = 0
 
-    with open(request, mode="r") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
+    try:
+        with open(request, mode="r") as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
 
-        print("\nRequested Products: ")
-        for row in reader:
-            product_number = row[0]
-            quantity = int(row[1])
+            print("\nRequested Products: ")
+            for row in reader:
+                try:
+                    product_number = row[0]
+                    quantity = int(row[1])
+                except (IndexError, ValueError) as error:
+                    print(f"Error processing row {row}: {error}")
+                    continue  # Skip to next row
 
-            # cross-reference request with products_dict
-            if product_number in products_dict:
-                product_details = products_dict[product_number]
-                product_name = product_details[1]
-                product_price = Decimal(product_details[2])
-                product_line_cost = product_price * (quantity)
-                subtotal += product_line_cost
+                total_count += quantity
 
-                # Prints Receipt Line based on quantity
-                if quantity == 1:
-                    print(f"{product_name}: ${product_price:.2f}")
-                else:
+                try:
+                    # cross-reference request with products_dict
+                    product_details = products_dict[product_number]
+                    product_name = product_details[1]
+                    product_price = Decimal(product_details[2])
+                    product_line_cost = product_price * (quantity)
+                    subtotal += product_line_cost
+
+                    # Prints Receipt Line based on quantity
+                    if quantity == 1:
+                        print(f"{product_name}: ${product_price:.2f}")
+                    else:
+                        print(
+                            f"{quantity} {product_name}: ${product_line_cost:.2f} (${product_price:.2f} each)"
+                        )
+                except KeyError:
                     print(
-                        f"{quantity} {product_name}: ${product_line_cost:.2f} (${product_price:.2f} each)"
+                        f"Product number {product_number} not found. Please see cashier."
                     )
-            else:
-                print(f"Product number {product_number} not found. Please see cashier.")
+                    continue  # Skip to next row
+                except (IndexError, ValueError) as error:
+                    print(
+                        f"Error retrieving product data for {product_number}: {error}"
+                    )
+                    continue  # Skip to next row
 
-    sales_tax = subtotal * (Decimal(sales_tax_percentage) / 100)
-    total = subtotal + sales_tax
+                # Check for special discounts
+                if special_discounts is not None:
+                    if product_number in special_discounts:
+                        try:
+                            discount_details = special_discounts[product_number]
+                            sale_price = Decimal(discount_details[2])
+                            sale_line_cost = sale_price * quantity
+                            savings_amount = sale_line_cost - product_line_cost
+                            subtotal += savings_amount
+                            discount_total += savings_amount
 
-    # Print a blank line for readability.
-    print()
+                            if quantity == 1:
+                                print(
+                                    f"Sale Price: ${sale_price:.2f}, Total ${savings_amount:.2f}!"
+                                )
+                            else:
+                                print(
+                                    f"Sale Price: ${sale_price:.2f} each, Total ${savings_amount:.2f}!"
+                                )
 
-    print(f"Subtotal: ${subtotal:.2f}")
-    print(f"Sales tax: ${sales_tax:.2f}")
-    print(f"Total: ${total:.2f}")
+                        except (IndexError, ValueError) as error:
+                            continue  # Skip to next row
+
+        sales_tax = subtotal * (Decimal(sales_tax_percentage) / 100)
+        total = subtotal + sales_tax
+
+        # Print a blank line for readability.
+        print()
+
+        # Print the receipt
+        print(f"Number of Items: {total_count}")
+        print(f"Subtotal: ${subtotal:.2f}")
+        if discount_total < 0:
+            print(f"YOU SAVED: ${abs(discount_total):.2f}!")
+        print(f"Sales tax: ${sales_tax:.2f}")
+        print(f"Total: ${total:.2f}")
+
+    except FileNotFoundError:
+        print(f"File {request} not found. Please check the filename and try again.")
+        return
 
 
 def main():
+    # Print a blank line for readability.
+    print()
+
+    # Print store name at the top of the receipt
+    print(f"{STORE_NAME}")
+
     filename = "products.csv"
     key_column_index = 0
     products_dict = read_dictionary(filename, key_column_index)
+    if products_dict is None:
+        print()
+        print("We're sorry, but we are having technical difficulties at the moment.")
+        print(
+            "Please present this receipt to the manager who can manually check you out with a 20% discount on your entire purchase."
+        )
+        print()
+        print(f"{SIGNATURE_LINE}")
+        print(f"{current_date_and_time:%A %I:%M %p}")
+        return  # Exit program if products can't be loaded
 
-    # Print dictionary for debugging
-    print("Products Dictionary: ", products_dict)
+    # Print dictionary for debugging - COMMENT OUT WHEN DONE
+    # print("Products Dictionary: ", products_dict)
+
+    # Attempt to read the special_discounts.csv file. If it cannot be read,
+    # the program will continue without it.
+    discount_filename = "special_discounts.csv"
+    special_discounts = read_dictionary(discount_filename, key_column_index)
 
     # Open request.csv
     request = "request.csv"
-    read_request(request, products_dict)
+    read_request(
+        request,
+        products_dict,
+        sales_tax_percentage=SALES_TAX,
+        special_discounts=special_discounts,
+    )
 
     # Print a blank line for readability.
     print()
 
-    print("Thank you for shopping with us! Have a great day!")
+    print(f"{SIGNATURE_LINE}")
+    print(f"{current_date_and_time:%A %I:%M %p}")
 
 
 # Standard convention for running program, while leaving place for validation tests.
