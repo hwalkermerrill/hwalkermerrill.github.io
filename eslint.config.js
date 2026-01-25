6feed85cc8f05bb118156237e9ae94d9ab2fa545
Harrison Merrill <hwalkermerrill@gmail.com>
2026-01-25 16:21:57 +0900

    initialize lint and packaging

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      "no-console": "warn",
+    },
+  },
+]);
