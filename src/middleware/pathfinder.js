b171b3df3ee8b61c76bf46ff2efa0846bca10261
Harrison Merrill <hwalkermerrill@gmail.com>
2026-04-01 16:03:11 +0900

    set pathfinder.js to head as middleware

--- src/middleware/pathfinder.js
+++ src/middleware/pathfinder.js
@@ -3,31 +3,31 @@
 
 // This block handles the current date and version numbers displayed in the footer.
 // [Start Date and Version Block]
-const today = new Date();
-const yyyy = today.getFullYear();
-const versionIteration = "v1.6.2."
+// const today = new Date();
+// const yyyy = today.getFullYear();
+// const versionIteration = "v1.6.2."
 
-function writeLongDate() { // eslint-disable-line no-unused-vars
-	let dwArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	let mmArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// function writeLongDate() { // eslint-disable-line no-unused-vars
+// 	let dwArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// 	let mmArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-	let dw = dwArray[today.getDay()];
-	let mm = mmArray[today.getMonth()];
-	let dd = today.getDate();
-	if (dd < 10) { dd = "0" + dd }
+// 	let dw = dwArray[today.getDay()];
+// 	let mm = mmArray[today.getMonth()];
+// 	let dd = today.getDate();
+// 	if (dd < 10) { dd = "0" + dd }
 
-	let longDate = `${dw}, ${dd} ${mm} ${yyyy}`;
-	document.getElementById("longDate").textContent = longDate;
-}
+// 	let longDate = `${dw}, ${dd} ${mm} ${yyyy}`;
+// 	document.getElementById("longDate").textContent = longDate;
+// }
 
-function writeVersionNumber() { // eslint-disable-line no-unused-vars
-	let yy = yyyy.toString().slice(2);
-	let mn = today.getMonth() + 1;
-	if (mn < 10) { mn = "0" + mn }
+// function writeVersionNumber() { // eslint-disable-line no-unused-vars
+// 	let yy = yyyy.toString().slice(2);
+// 	let mn = today.getMonth() + 1;
+// 	if (mn < 10) { mn = "0" + mn }
 
-	let versionNumber = versionIteration + yy + mn;
-	document.getElementById("versionNumber").textContent = versionNumber;
-}
+// 	let versionNumber = versionIteration + yy + mn;
+// 	document.getElementById("versionNumber").textContent = versionNumber;
+// }
 // [End Date and Version Block]
 
 // These next two are depreciated but preserved for reference.
